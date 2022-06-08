9396f66b7114850f92efac2b2bc28c827048eab1
chahyeonnaa <gusdk21650@naver.com>
2022-06-09 01:02:20 +0900

    [FEAT] 배너 뼈대

--- product/freshproduct/buy.js
+++ product/freshproduct/buy.js
@@ -1,4 +1,4 @@
-function selectEmail(ele){
+function selectEmail1(ele){
     var $ele = $(ele);
     var $email2 = $('input[name=email2]');
 
@@ -11,7 +11,88 @@ function selectEmail(ele){
     }
 }
 
+function selectEmail2(ele){
+    var $ele = $(ele);
+    var $email2 = $('input[name=email2]');
+
+    if($ele.val() == "1"){
+        $email2.attr('readonly', false);
+        $email2.val('');
+    } else {
+        $email2.attr('readonly', true);
+        $email2.val($ele.val());
+    }
+}
+
+function selectEmail3(ele){
+    var $ele = $(ele);
+    var $email2 = $('input[name=email2]');
+
+    if($ele.val() == "1"){
+        $email2.attr('readonly', false);
+        $email2.val('');
+    } else {
+        $email2.attr('readonly', true);
+        $email2.val($ele.val());
+    }
+}
+
+function selectEmail4(ele){
+    var $ele = $(ele);
+    var $email2 = $('input[name=email2]');
+
+    if($ele.val() == "1"){
+        $email2.attr('readonly', false);
+        $email2.val('');
+    } else {
+        $email2.attr('readonly', true);
+        $email2.val($ele.val());
+    }
+}
 function order(){
 
-// 체크박스 내용 읽어서 결제 수단 알람으로 띄우기
+  var checkbox=document.getElementsByName("boxes");
+  var msg="결제 정보";
+
+  for(var i=0;i<checkbox.length;i++)
+  {
+    if(checkbox[i].checked)
+    {
+      if(i==0)
+      {
+        var s= document.getElementById("sel2");
+        var v= document.getElementById("sel3");
+        var x= document.getElementById("cardnum1").value;
+        var cont=s.options[s.selectedIndex].value;
+        var cons=v.options[v.selectedIndex].value;
+        var st="신용카드"
+        alert(msg+'\n'+'결제수단: '+st+'\n'+cont+'/'+cons+'/'+x);
+      }
+      else if(i==1)
+      {
+        var s= document.getElementById("sel4");
+        var cont=s.options[s.selectedIndex].value;
+        var st="무통장입금"
+        alert(msg+'\n'+'결제수단: '+st+'\n'+cont);
+
+      }
+      else if(i==2)
+      {
+        var s= document.getElementById("sel5");
+        var cont=s.options[s.selectedIndex].value;
+        var st="계좌이체"
+        alert(msg+'\n'+'결제수단: '+st+'\n'+cont);
+      }
+      else if(i==3)
+      {
+        var s= document.getElementById("sel6");
+        var cont=s.options[s.selectedIndex].value;
+        var x= document.getElementById("phone").value;
+        var st="휴대폰 결제"
+        alert(msg+'\n'+'결제수단: '+st+'\n'+cont+'/'+x);
+      }
+      }
+
+  }
+
 }
--- product/freshproduct/product.js
+++ product/freshproduct/product.js
@@ -14,7 +14,6 @@ function Buying(){
   alert('구매상품 : '+name+'\n'+'개수 : '+num+'\n'+'합계 : '+sum+'\n');
 }
 
-// 버튼 누를 때 마다 상품 정보들을 어딘가에 담아야함
 function Carting(){
   var name=document.getElementById("product").innerHTML;
   var sum=document.getElementById("private").innerHTML;
